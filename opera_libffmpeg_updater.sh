238d49d99254c8f090c9ceab219281bb201051cd
xXMacMillanXx <xXMacMillanXx@gmx.at>
2024-07-03 18:22:33 +0200

    Remove double API call

--- opera_libffmpeg_updater.sh
+++ opera_libffmpeg_updater.sh
@@ -1,10 +1,11 @@
 # download for backups in case something breaks, creates folders if not exist
 mkdir -p ~/Downloads/libffmpeg_web
-curl -s https://api.github.com/repos/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases/latest \
+json=$(curl -s https://api.github.com/repos/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases/latest)
+echo $json \
 | jq -r .assets[1].browser_download_url \
 | wget -qi - --directory-prefix ~/Downloads/libffmpeg_web
 # actual download to replace libffmpeg in opera directory
-curl -s https://api.github.com/repos/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases/latest \
+echo $json \
 | jq -r .assets[1].browser_download_url \
 | wget -qi - -O temp.zip
 # handle downloaded file
